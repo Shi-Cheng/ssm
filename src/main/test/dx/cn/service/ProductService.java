425f708ead9b9b687d789ebb407e884ffc415eb4
Shi-Cheng <x_pan2016@163.com>
2019-03-22 14:47:59 +0800

    1.0.3 数据库部署在虚拟机

--- src/main/test/dx/cn/service/ProductService.java
+++ src/main/test/dx/cn/service/ProductService.java
@@ -5,11 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class ProductService {
 
+    private Long phone;
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
     @Autowired IProductService productService;
     @Test
     public void findProductAll(){
         String name = "user";
         String name1 ="user";
         System.out.println(name.equals(name1));
+    }
+
+
+    public void testResult(){
+
+
     }
 }
